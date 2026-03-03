71919ac8b5d4ec051e96eab656097ed351207afd
guimartins022 <guimartinsoliveira022@gmail.com>
2026-03-02 21:51:26 -0300

    Exercício de composição (poo).

---
+++ desafioComposicao/src/desafioComposicao/Compra.java
@@ -0,0 +1,28 @@
+package desafioComposicao;
+
+import java.util.ArrayList;
+
+public class Compra {
+
+	final ArrayList<Item> itens = new ArrayList<>(); // Uma compra tem vários itens.
+	
+	void adicionarItem(Produto p, int quantidade) {
+		this.itens.add(new Item(p, quantidade));
+	}
+	
+	void adicionarItem(String nome, double preco, int quantidade) {
+		var produto = new Produto(nome, preco);
+		this.itens.add(new Item(produto, quantidade));
+	}
+	
+	double obterValorTotal() {
+		double total = 0;
+		
+		for(Item item: itens) {
+			total += item.quantidade * item.produto.preco;
+		}
+		
+		return total;
+	}
+	
+}
---
+++ desafioComposicao/src/desafioComposicao/Produto.java
@@ -0,0 +1,13 @@
+package desafioComposicao;
+
+public class Produto {
+
+	String nome;
+	double preco;
+	
+	Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+}
